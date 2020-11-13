95b9a7d9345381ea89ac2d329e2ea7f16c6f1b12
ChaminduNisasala <33674197+ChaminduN@users.noreply.github.com>
2020-11-13 06:22:33 +0530

    After the Migrate (.androidx ) ( 11-13-20 )

--- app/src/main/java/com/example/Constructo/AdminCategoryActivity.java
+++ app/src/main/java/com/example/Constructo/AdminCategoryActivity.java
@@ -1,6 +1,6 @@
 package com.example.Constructo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AdminCategoryActivity extends AppCompatActivity {
